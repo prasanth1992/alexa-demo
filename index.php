9a35b24bbfd5402916b8e1e214b23cc60f4cc448
prasanth <battuprasanth1993@gmail.com>
2018-08-30 12:56:49 +0530

    index

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
+if($RequestMessageType == "LaunchRequest"){
 $text = "Welcome World This is my First Https app using webhook";
 $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
 echo json_encode($array);
-
+}
 ?>

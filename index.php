46f2018cb3a65a3580911b86f0bdff1dccc7aa14
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-08-31 12:59:47 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
 $rawJSON = file_get_contents('php://input');
 $EchoReqObj = json_decode($rawJSON);
-if($EchoReqObj->request->type="jdjdjd"){
 $text = "Welcome World This is my First Https app using webhook";
+$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
+echo json_encode($array);
+if($EchoReqObj->request->intent->name="New"){
+$text = "Welcome New";
 $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
 echo json_encode($array);
 }

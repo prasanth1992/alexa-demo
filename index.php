0400d9ce264eae16e2274edb051ba8d49b825214
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-09-04 12:34:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,12 +8,12 @@ echo json_encode($array);
 }
 else if($EchoReqObj->request->intent->name =="New"){
 $text = "Welcome World This New";
-$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
+$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
 echo json_encode($array);
 }
 else if($EchoReqObj->request->intent->name =="latest"){
 $text = "Welcome World This latest";
-$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text)));
+$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
 echo json_encode($array);
 }
 

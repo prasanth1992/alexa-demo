33190621644d8378e47520ba3bde3c31810d7e9a
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-09-14 12:00:13 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,34 +2,34 @@
 $rawJSON = file_get_contents('php://input');
 $EchoReqObj = json_decode($rawJSON);
 if($EchoReqObj->request->type=="LaunchRequest"){
-$text = "Welcome World This is my First Https app using webhook";
-$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
-echo json_encode($array);
-}
-else if($EchoReqObj->request->intent->name =="New"){
-$text = "Welcome World This New";
-$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
-echo json_encode($array);
-}
-else if($EchoReqObj->request->intent->name =="latest"){
-$text = "Welcome World This latest";
-$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
-echo json_encode($array);
-}
-else if($EchoReqObj->request->intent->name =="today"){
+  $text = "Welcome World This is my First Https app using webhook";
+  $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
+  echo json_encode($array);
+ }
+ else if($EchoReqObj->request->intent->name =="New"){
+    $text = "Welcome World This New";
+    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
+    echo json_encode($array);
+ }
+  else if($EchoReqObj->request->intent->name =="latest"){
+    $text = "Welcome World This latest";
+    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
+    echo json_encode($array);
+ }
+  else if($EchoReqObj->request->intent->name =="today"){
   
-$ch = curl_init('http://ec2-34-228-218-131.compute-1.amazonaws.com/AlexaIvanti/Api/Incident/GetCountOfTodaysIncident');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    $ch = curl_init('http://ec2-34-228-218-131.compute-1.amazonaws.com/AlexaIvanti/Api/Incident/GetCountOfTodaysIncident');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+  
+    $text=curl_exec($ch);
+    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"Today incidents". $text),"shouldEndSession"=>false));
+    echo json_encode($array);
+    curl_close($ch);
+ }
 
-$text=curl_exec($ch);
-  $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"Today incidents". $text),"shouldEndSession"=>false));
-echo json_encode($array);
-curl_close($ch);
-}
-
-else if($EchoReqObj->request->intent->name =="status"){
-  $text="Enter the user id";
-$array = array ('version' => '1.0','response' => array ('outputSpeech' => array ('type' => 'PlainText','text' => $text,),'directives' => 
+  else if($EchoReqObj->request->intent->name =="status"){
+    $text="Enter the user id";
+    $array = array ('version' => '1.0','response' => array ('outputSpeech' => array ('type' => 'PlainText','text' => $text,),'directives' => 
     array (
       0 => 
       array (
@@ -38,20 +38,16 @@ $array = array ('version' => '1.0','response' => array ('outputSpeech' => array 
       ),
     ),
     'shouldEndSession' => false,
-  ),
-);
-  echo json_encode($array);
- if ($EchoReqObj->request->intent->slots->id->value == "123"){
+    ),
+    );
+    echo json_encode($array);
+}
+ if ($EchoReqObj->request->intent->name =="status" && $EchoReqObj->request->intent->slots->id->value == "123"){
  $text = "respose";
 $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
 echo json_encode($array);
 }
-  else{
- $text = "error";
-$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
-echo json_encode($array);
-}
-}
+
 
 
 

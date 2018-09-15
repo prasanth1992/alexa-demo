8d6a702846d65bfe5853748502a3f04b08bab758
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-09-15 16:16:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ $data_string = json_encode($data);
 $ch = curl_init('http://ec2-34-228-218-131.compute-1.amazonaws.com/AlexaIvanti/Api/Incident/CreateIncident');                                                                      
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);                                                                      
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
 curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
     'Content-Type: application/json'                                                                              
                                                                           

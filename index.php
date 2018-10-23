cac82baae48dd2d6ed34ba66379b86f0524278ee
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-10-23 14:43:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -107,18 +107,12 @@ else if($EchoReqObj->request->intent->name =="AMAZON.RepeatIntent"){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   
     $text=curl_exec($ch);
-     if($text=""){
-       $text="No Summary Today";
+     
+       
        $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"Today incidents". $text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
     echo json_encode($array);
     curl_close($ch);
-     }
-     else{
-        $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"Today incidents". $text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
-    echo json_encode($array);
-    curl_close($ch);
-     
-     }
+   
     
  }
 

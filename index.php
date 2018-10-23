e469a6e110acb1defc7a7559e845a78073b38614
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-10-23 16:21:32 +0530

    Update index.php

--- index.php
+++ index.php
@@ -109,21 +109,43 @@ else if($EchoReqObj->request->intent->name =="AMAZON.RepeatIntent"){
     $text=curl_exec($ch);
     if($text='""'){
       $text="No summary Today";
-    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"Today incidents". $text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
+    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
     echo json_encode($array);
        curl_close($ch);
     }
     else{
-    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"Today incidents". $text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
+    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
     echo json_encode($array);
     curl_close($ch);
     }
+  }
+  else if($EchoReqObj->request->intent->name == "closeincident"){
+    $text="Enter the close incident id";
+    $array = array ('version' => '1.0','response' => array ('outputSpeech' => array ('type' => 'PlainText','text' => $text,),'directives' => 
+    array (
+      0 => 
+      array (
+        'type' => 'Dialog.ElicitSlot',
+        'slotToElicit' => 'id',
+      ),
+    ),
+    'shouldEndSession' => false,
+    ),
+    );
+ if ($var=$EchoReqObj->request->intent->slots->id->value){
+
+    $ch = curl_init('http://ec2-34-228-218-131.compute-1.amazonaws.com/AlexaIvanti/Api/Incident/CloseIncident/'.$var);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   
-       
+    $text=curl_exec($ch);
+    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"Today incidents". $text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
+    echo json_encode($array);
+    curl_close($ch);
    
    
-    
- }
+}
+  echo json_encode($array);
+}
 
 
 

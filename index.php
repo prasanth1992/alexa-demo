38ab0cdcdfcafbc7a49be08f0443335b4ca12428
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-10-30 13:53:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -54,8 +54,12 @@ if($EchoReqObj->request->type=="LaunchRequest"){
 }
   echo json_encode($array);
 }
+// Test of Create object
+
  else if($EchoReqObj->request->intent->name == "CreateObject"){
-    $text="please enter Subject of Incident";
+	 
+	 else if(($EchoReqObj->request->intent->slots=="subject"){
+	$text="please enter Subject of Incident";
     $array = array ('version' => '1.0','response' => array ('outputSpeech' => array ('type' => 'PlainText','text' => $text,),'directives' => 
     array (
       0 => 
@@ -67,28 +71,45 @@ if($EchoReqObj->request->type=="LaunchRequest"){
     'shouldEndSession' => false,
     ),
     );
- if ($var=$EchoReqObj->request->intent->slots->subject->value){
+	 if ($var=$EchoReqObj->request->intent->slots->subject->value){
    
-$data = array("subject" => $var);                                                                    
-$data_string = json_encode($data);                                                                                   
-$ch = curl_init('http://ec2-34-228-218-131.compute-1.amazonaws.com/AlexaIvanti/Api/Incident/CreateIncident');                                                                      
-curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
-curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-    'Content-Type: application/json'                                                                              
-                                                                          
-));                                                                                                                   
-                                                                                                                     
-    $result = curl_exec($ch);
-    curl_close($ch);                                                                                                                     
-
-    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$result),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$result));
-    echo json_encode($array);
+$subject = array("subject" => $var);                                                                    
+$subject_string = json_encode($data);                                                                                   
+  
   
 }
+	 }
+	 else if(($EchoReqObj->request->intent->slots=="subject"){
+	$text="please enter Subject of Incident";
+    $array = array ('version' => '1.0','response' => array ('outputSpeech' => array ('type' => 'PlainText','text' => $text,),'directives' => 
+    array (
+      0 => 
+      array (
+        'type' => 'Dialog.ElicitSlot',
+        'slotToElicit' => 'description',
+      ),
+    ),
+    'shouldEndSession' => false,
+    ),
+    );
+	 if ($var=$EchoReqObj->request->intent->slots->subject->value){
+   
+$description = array("subject" => $var);                                                                    
+$description_string = json_encode($data);                                                                                   
+  
+  
+}
+	 }
+  $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>"subject is".$subject."description is".$description),"shouldEndSession"=>false));
+
+	 
+  
+
   echo json_encode($array);
 }
+
+
+//end of test
 
  else if($EchoReqObj->request->intent->name =="AMAZON.StopIntent"){
     $text = "Hmm Ok ";

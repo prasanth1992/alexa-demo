4bd8c0d7a6b218474a94a9bd6022dba9c66e1efb
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-10-31 11:35:24 +0530

    Update index.php

--- index.php
+++ index.php
@@ -56,8 +56,6 @@ if($EchoReqObj->request->type=="LaunchRequest"){
 }
 
   else if($EchoReqObj->request->intent->name == "CreateObject"){
-    
-   if($EchoReqObj->request->intent->slot->subject->name=="subject"){
     $text="please enter Subject of Incident";
     $array = array ('version' => '1.0','response' => array ('outputSpeech' => array ('type' => 'PlainText','text' => $text,),'directives' => 
     array (
@@ -70,10 +68,26 @@ if($EchoReqObj->request->type=="LaunchRequest"){
     'shouldEndSession' => false,
     ),
     );
- $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$result),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$result));
- echo json_encode($array);
+ if ($var=$EchoReqObj->request->intent->slots->subject->value){
+   
+$data = array("subject" => $var);                                                                    
+$data_string = json_encode($data);                                                                                   
+$ch = curl_init('http://ec2-34-228-218-131.compute-1.amazonaws.com/AlexaIvanti/Api/Incident/CreateIncident');                                                                      
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
+curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);                                                                      
+curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+    'Content-Type: application/json'                                                                              
+                                                                          
+));                                                                                                                   
+                                                                                                                     
+    $result = curl_exec($ch);
+    curl_close($ch);                                                                                                                     
+
+    $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$result),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$result));
+    echo json_encode($array);
+  
 }
-   }
   echo json_encode($array);
 }
 

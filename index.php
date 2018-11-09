23b86ff86457c56d59ab33686760604c3f6e8845
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-11-09 17:45:28 +0530

    Update index.php

--- index.php
+++ index.php
@@ -165,13 +165,13 @@ else if($EchoReqObj->request->intent->name =="AMAZON.RepeatIntent"){
       $text="No summary Today";
     $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
     echo json_encode($array);
-       curl_close($ch);
     }
     else{
     $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
     echo json_encode($array);
-    curl_close($ch);
     }
+	       curl_close($ch);
+
   
   }
   /* End of Today Summary*/

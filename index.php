3596c01f82929962801de9c75d802ca484d003d9
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-11-20 15:28:50 +0530

    Update index.php

--- index.php
+++ index.php
@@ -247,7 +247,7 @@ else if($EchoReqObj->request->intent->name =="AMAZON.RepeatIntent"){
 /* End of Services*/
 /* Else Statement*/
 
-  else if($rawJSON=='' || $rawJSON==null){
+  else if($EchoReqObj->request->intent->name == "AMAZON.FallbackIntent"){
   $text = "In else part";
   $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
   echo json_encode($array);

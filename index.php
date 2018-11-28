58354681b9281b8d8f653b76526039c2c2ea3242
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-11-28 20:37:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -280,5 +280,16 @@ else if($EchoReqObj->request->intent->name =="AMAZON.RepeatIntent"){
 
 /* End of handle*/
 
+/* NUll value*/
+
+  else if($EchoReqObj-> == "null"){
+  $text = "Sorry I didn't find that services, please try it again.";
+  $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
+  echo json_encode($array);
+}
+
+
+/* End of NUll value*/
+
 
 ?>

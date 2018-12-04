aa1f8b4c7cab09e78eafd3add9b3b9904ea18761
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-12-04 21:05:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ $EchoReqObj = json_decode($rawJSON);
 /* Launch Request*/
 if($EchoReqObj->request->type=="LaunchRequest"){
   $text = "Welcome to Ivanti Services. To know the services I can provide, please ask what I can do";
-  $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false));
+  $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"reprompt"=>array("type"=>"PlainText","text"=>"Didn't received Input"),"shouldEndSession"=>false));
   echo json_encode($array);
  }
  /* End of launch request*/

d93268a6d7810116ae91b9a1356792459ec4f27a
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-12-04 11:24:41 +0530

    Update index.php

--- index.php
+++ index.php
@@ -269,16 +269,7 @@ else if($EchoReqObj->request->intent->name =="AMAZON.RepeatIntent"){
 
 /* End of Else statement*
 
-/* Error Handle*/
 
-  else if($EchoReqObj->request->type == "SessionEndedRequest"){
-  $text = "No Response From You, Thanks for using me.";
-  $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>true));
-  echo json_encode($array);
-}
-
-
-/* End of handle*/
 
 
 /* NUll value*/

ad84dedd3e206c3360cd7c90a55f695c60bfab92
prasanth1992 <42199357+prasanth1992@users.noreply.github.com>
2018-12-24 13:08:27 +0530

    Update index.php

--- index.php
+++ index.php
@@ -195,9 +195,16 @@ else if($EchoReqObj->request->intent->name =="AMAZON.RepeatIntent"){
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
   
     $text=curl_exec($ch);
-	
-	 $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
-   	 echo json_encode($array);
+	 if (strpos($text, 'object is in final state') != false) {
+    		$text='Incident is already closed.';
+		$array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
+   	        echo json_encode($array);
+	}
+	 else{
+		 $array = array("version"=>"1.0","response"=>array("outputSpeech"=>array("type"=>"PlainText","text"=>$text),"shouldEndSession"=>false),"sessionAttributes"=>array("lastSpeech"=>$text));
+   		 echo json_encode($array);
+	 }
+	 
 	 
 	
     
